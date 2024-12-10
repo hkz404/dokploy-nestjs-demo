e069abd57a6ad83020e081e235a4c154624d0aec
hahnzhu <hahnzhu@tencent.com>
2024-12-10 11:12:37 +0800

    print port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   await app.listen(config.get('PORT', 3002));
+
+  console.log(`Application is running on: ${await app.getUrl()}\n`);
 }
+
+process.on('unhandledRejection', (reason) => {
+  console.log('🔥🔥🔥', reason);
+});
 
 bootstrap(); 
